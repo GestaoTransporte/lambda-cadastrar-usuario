c65bb84c19726164f6ce59ce43bf14d1c0505b77
Igor Saraiva Correa <igorcsaraiva@hotmail.com>
2024-04-25 20:09:46 -0300

    fix: adição de region

--- app/src/LambdaFunction/Ports/parameter_store.py
+++ app/src/LambdaFunction/Ports/parameter_store.py
@@ -3,7 +3,7 @@ from aws_lambda_powertools import Logger
 
 logger = Logger()
 
-client = boto3.client('ssm')
+client = boto3.client('ssm',region_name='sa-east-1')
 
 def get_parameter(name):
     logger.info(f"Obtendo parametro {name}")
