e1f87d0ababe942c4ca1f018c2b0ec23b3d7c6f3
Igor Saraiva Correa <igorcsaraiva@hotmail.com>
2024-04-30 11:52:16 -0300

    fix: teste

--- infra/main.tf
+++ infra/main.tf
@@ -11,67 +11,67 @@ module "lambda_function" {
   local_existing_package = "../package.zip"
 
   role_name = aws_iam_role.lambda_role.name
-  depends_on = [ aws_iam_role.lambda_role ]
+  # depends_on = [ aws_iam_role.lambda_role ]
 }
 
-resource "aws_iam_role" "lambda_role" {
-  name = "${var.function_name}_role"
+# resource "aws_iam_role" "lambda_role" {
+#   name = "${var.function_name}_role"
 
-  assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Action"    : "sts:AssumeRole",
-        "Principal" : {
-          "Service" : "lambda.amazonaws.com"
-        },
-        "Effect"    : "Allow",
-        "Sid"       : ""
-      }
-    ]
-  })
-}
+#   assume_role_policy = jsonencode({
+#     "Version" : "2012-10-17",
+#     "Statement" : [
+#       {
+#         "Action"    : "sts:AssumeRole",
+#         "Principal" : {
+#           "Service" : "lambda.amazonaws.com"
+#         },
+#         "Effect"    : "Allow",
+#         "Sid"       : ""
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_policy" "lambda_policy" {
-  name        = "${var.function_name}_policy"
-  description = "Policy for Lambda function"
+# resource "aws_iam_policy" "lambda_policy" {
+#   name        = "${var.function_name}_policy"
+#   description = "Policy for Lambda function"
 
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ec2:CreateNetworkInterface",
-                "ec2:DescribeNetworkInterfaces",
-                "ec2:DeleteNetworkInterface"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "logs:CreateLogGroup",
-                "logs:CreateLogStream",
-                "logs:PutLogEvents"
-            ],
-            "Resource": "arn:aws:logs:*:*:*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ec2:DescribeVpcs",
-                "ec2:DescribeSubnets",
-                "ec2:DescribeSecurityGroups",
-                "rds-db:connect"
-            ],
-            "Resource": "*"
-        }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     "Version" : "2012-10-17",
+#     "Statement" : [
+#         {
+#             "Effect": "Allow",
+#             "Action": [
+#                 "ec2:CreateNetworkInterface",
+#                 "ec2:DescribeNetworkInterfaces",
+#                 "ec2:DeleteNetworkInterface"
+#             ],
+#             "Resource": "*"
+#         },
+#         {
+#             "Effect": "Allow",
+#             "Action": [
+#                 "logs:CreateLogGroup",
+#                 "logs:CreateLogStream",
+#                 "logs:PutLogEvents"
+#             ],
+#             "Resource": "arn:aws:logs:*:*:*"
+#         },
+#         {
+#             "Effect": "Allow",
+#             "Action": [
+#                 "ec2:DescribeVpcs",
+#                 "ec2:DescribeSubnets",
+#                 "ec2:DescribeSecurityGroups",
+#                 "rds-db:connect"
+#             ],
+#             "Resource": "*"
+#         }
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "lambda_attach_policy" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.lambda_policy.arn
-}
+# resource "aws_iam_role_policy_attachment" "lambda_attach_policy" {
+#   role       = aws_iam_role.lambda_role.name
+#   policy_arn = aws_iam_policy.lambda_policy.arn
+# }
